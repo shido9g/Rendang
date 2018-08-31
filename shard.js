80077cb511d171421c6cae3bf6850304d8407e80
Hazmi35 <hazminoname@gmail.com>
2018-08-31 18:47:35 +0700

    Hello World

---
+++ shard.js
@@ -0,0 +1,16 @@
+const { ShardingManager } = require('discord.js');
+
+const shards = new ShardingManager('./index.js', {
+    token: TOKEN_GOES_HERE,
+    totalShards: 1
+});
+
+shards.on('launch', shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
+});
+
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
+
+shards.spawn();
