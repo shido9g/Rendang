b7b50c8683ad9a3362436e14d7caaf36ba215021
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-08-31 18:28:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const Discord = require('discord.js');
 // variable berjalan 
 var client = new Discord.Client();
 
+// Ready event
+client.on('ready', () => {
+  // ini bakal ke log di console
+  console.log(`${client.user.username} Sudah ONLINE`); // => ex: CodDev Sudah ONLINE
+ });
+
 // login ke discord 
 client.login('SUPER');
