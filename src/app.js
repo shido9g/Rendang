f1fae4da8a3965939123755046f4dad3a19408d2
Akizuki Masami <34865695+Masami45@users.noreply.github.com>
2018-08-31 18:05:58 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({
+const codDevClient = require('./handle/codDevClient');
+const client = new codDevClient({
   fetchAllMembers: true,
   disabledEvents: ["TYPING_START", "USER_NOTE_UPDATE"],
   disableEveryone: true
 });
 
-client.login(TOKEN_GOES_HERE);
-
 require('./handle/events')(client);
 require('./handle/module')(client);
+
+client.login(TOKEN_GOES_HERE);
