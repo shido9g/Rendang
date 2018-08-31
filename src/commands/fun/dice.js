7c5805d5861977cee3ee9d70caca3ccd5665325b
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-08-31 21:30:26 +0800

    Update dice.js

--- src/commands/fun/dice.js
+++ src/commands/fun/dice.js
@@ -1,14 +1,14 @@
 const { RichEmbed } = require('discord.js');
 
 exports.run = (client, message, args, color) => {
+  
+  let roll = = Math.floor(Math.random() * args.xsides) + 1;
   const embed = new RichEmbed() 
   .setColor(color)
   .setThumbnail('https://gilkalai.files.wordpress.com/2017/09/dice.png?w=640')
   .setAuthor(message.author.username, message.author.avatarURL)
-  .setTitle('ROLED DICE')
-  .setDescription(`
-${Math.floor(Math.random()*8)}
-`);
+  .setTitle("Rolling a " + args.xsides + " sided dice.")
+  .setDescription("You roled a" + roll)
   return message.channel.send(embed);
 }
 
