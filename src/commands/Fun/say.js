7642333230d882419243e78d1a960a907f5eb8f3
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-08-31 18:47:03 +0700

    Create say.js

---
+++ src/commands/Fun/say.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+  var botmessage = args.join(" ");
+  if(!botmessage) {
+    var embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setDescription(`<@${message.author.id}> Saya Harus Ngomong Apa?`)
+    .setTimestamp()
+    
+    message.channel.send({embed});
+  }
+	message.delete();
+	message.channel.send(botmessage);
+  
+}
+  
+  exports.help = {
+    name: 'say',
+    aliases: ['s'],
+    description: 'Makes Bot Say Something',
+    usage: '(p)say [message]'
+  };
