c18acd499af1c48482c24a2a33adca13021dcf59
Hazmi35 <hazminoname@gmail.com>
2018-08-31 17:34:25 +0700

    K

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// memanggil package 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-// variable berjalan 
-var client = new Discord.Client({ disableEveryone: true }); //jadi botnya gk bakal bisa tag @everyone
-
-// Ready event
-client.on('ready', () => {
-  // ini bakal ke log di console
-  console.log(`${client.user.username} Sudah ONLINE`); // => ex: CodDev Sudah ONLINE
- });
-
-// login ke discord 
-client.login('SUPER');
- 
+var client = new Discord.Client({
+  disableEveryone: true,
+  disabledEvents: [
+    "TYPING_START",
+    "USER_NOTE_UPDATE"
+  ]
+});
