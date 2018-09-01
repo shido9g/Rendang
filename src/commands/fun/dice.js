f1396f6b830e8697eedce6abf46f879fe966fb11
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-09-01 17:22:22 +0800

    Holly sh*t

--- src/commands/fun/dice.js
+++ src/commands/fun/dice.js
@@ -2,10 +2,11 @@ const { RichEmbed } = require('discord.js');
 
 exports.run = (client, message, args, color) => {
   
-  let number = args.join(' ');
-  if (!number) number = 6;
+  let number = args.join('');
+  
   if(isNaN(args[0])) return message.channel.send('You only can use a number in a dice');
   
+  if (!number) number = 6;
   let roll = Math.floor(Math.random() * number) + 1;
   const embed = new RichEmbed() 
   .setColor(color)
