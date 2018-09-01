327ecfcb0e90386a7a48f7fc01f7de97f05a6ad0
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-01 15:42:01 +0700

    Create cpp.js

---
+++ src/commands/utility/cpp.js
@@ -0,0 +1,29 @@
+const { post } = require('superagent');
+const { RichEmbed } require('discord.js');
+
+function cppRunner (client, msg, args){
+	args = args.slice(1).join(' ').trim()
+	if(!args) return msg.reply('please add some code');
+	const { text } = post('http://coliru.stacked-crooked.com/compile')
+	.send({
+		cmd: 'g++ main.cpp && ./a.out',
+		src: args
+	});
+	const embed = new RichEmbed()
+	.setColor('PURPLE')
+	.setDescription(`\`\`\`diff\n${text}\`\`\``);
+	return msg.reply(embed);
+}
+
+this.conf = {
+	aliases: ['cpp'],
+	cooldown: 10
+}
+
+this.help = {
+	name: 'c++',
+	description: 'Compile your c++ code!',
+	usage: 'c++ [code]'
+}
+
+this.run = cppRunner;
