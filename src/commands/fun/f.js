79295ae2d3dc7f9332a2ca475d4ab39594db3b63
Hazmi35 <hazminoname@gmail.com>
2018-09-01 17:07:43 +0700

    FIXED IT!

--- src/commands/fun/f.js
+++ src/commands/fun/f.js
@@ -3,7 +3,32 @@ const path = require("path");
 const SQL = require('sqlite3').verbose();
 
 exports.run = async (client, message, args, color) => {
-    //const database = new SQL.Database(path.join(__dirname, '..', '..', 'databases', 'f-total.db'));
+    const database = new SQL.Database(path.join(__dirname, '..', '..', 'databases', 'f-total.db'));
+    var db = await database;
+
+    db.run("CREATE TABLE IF NOT EXISTS main (total INTEGER)");
+    db.get(`SELECT * FROM main`, async function (err, rows) {
+        if (err) {
+            if (err.message === "SQLITE_ERROR: no such table: main") return undefined;
+            console.error(err.message)
+        }
+        try {
+            var fTotal = rows.total + 1;
+            db.run(`UPDATE main SET total = '${fTotal}'`)
+        } catch (e) {
+            if (e.message === "Cannot read property 'total' of undefined") {
+                db.run("INSERT INTO main (total) VALUES (?)", [0]);
+                fTotal = 0
+            } else {
+                console.error(e)
+            }
+        }
+        var embed = new Discord.RichEmbed()
+            .setDescription(`${message.author.tag} Has paid their respects!`)
+            .setFooter(fTotal + " Respects")
+            .setColor(color)
+        message.channel.send(embed)
+    });
 
 }
 
