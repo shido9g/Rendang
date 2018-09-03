ac87a6fc578392ba47e11c92cc7239d1bc01c62c
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-09-03 11:50:43 +0700

    Create serverinfo.js

---
+++ src/commands/utility/serverinfo.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+
+let sicon = message.guild.iconURL; // kalau server gunakan icon bukan displayAvatar
+		let serverembed = new Discord.RichEmbed()
+		.setAuthor("Server Information :")
+		.setColor("RANDOM")
+		.setThumbnail(sicon)
+		.addField("Server Name", message.guild.name)
+		.addField("Created At", message.guild.createdAt) 
+		.addField("You Joined At", message.member.joinedAt)
+		.addField("Owner", message.guild.owner)
+    .setFooter(`Requested By : ${message.author.tag}`);
+
+		message.channel.send(serverembed);
+    
+   }
+
+   exports.conf = {
+     aliases: ['server-info'],
+     cooldown: '5'
+   
+    }
+    
+    exports.help = {
+      name: "help",
+      description: 'Melihat Info Server'
+      usage: 'serverinfo'
+    }
