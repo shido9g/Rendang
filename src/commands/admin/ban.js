930d7be312f9b7e94b4a14ddb39d86eec4b6885e
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-09-03 13:46:18 +0700

    Update ban.js

--- src/commands/admin/ban.js
+++ src/commands/admin/ban.js
@@ -7,25 +7,25 @@ exports.run = async (client, message, args) => {
       .setTitle("Sorry You No Have Permissions To Ban Members");
 return message.channel.send(embed);
   }
-  if (!message.guild.member(client.user).hasPermission("MANAGE_NICKNAMES")) return message.channel.send(`**${message.author.tag}** Sorry, I don't have \`BAN_MEMBERS\` permissions.`).then(msg=>msg.delete(5000))
+  if (!message.guild.member(client.user).hasPermission("MANAGE_NICKNAMES")) return message.channel.send(`**${message.author.tag}** Maaf, Rendang Tidak Mempunyai Permissions \`BAN_MEMBERS\` Tolong Beri Rendang Permissions Untuk Ban Member :)`).then(msg=>msg.delete(5000))
   
   let toBan = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!toBan) return message.channel.sendMessage("Can't find user! Please Mention User !");
+  if(!toBan) return message.channel.sendMessage("Tidak Dapat Menemukan User ! Mention User Terlebih Dahulu !");
   let reason = args.join(" ").slice(22);
-  if (toBan.hasPermission("BAN_MEMBERS")) return message.channel.send("That person can't be banned").then(msg => msg.delete(3000));
+  if (toBan.hasPermission("BAN_MEMBERS")) return message.channel.send("User Ini Tidak Dapat Diban :(").then(msg => msg.delete(3000));
   
   if (toBan.highestRole.position < message.guild.member(client.user).highestRole.position) {
    message.guild.member(toBan).ban(reason);
    try {
     if (!reason) {
-      toBan.send(`**${toBan.user.tag}** You were banned from **${message.guild.name}**`)
+      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari**${message.guild.name}**`)
     } else {
-      toBan.send(`**${toBan.user.tag}** You were banned from **${message.guild.name}**
-Reason: "${reason}"`);
+      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari**${message.guild.name}**
+Alasan: "${reason}"`);
     }
     let embedB = new RichEmbed()
     .setColor('RANDOM')
-    .setTitle('User Banned')
+    .setTitle('User Telah Diban Dari Server')
     .addField('username', toBan.user.username, true)
     .addField('ID', toBan.id, true)
     message.channel.send(embedB);
@@ -33,7 +33,7 @@ Reason: "${reason}"`);
     console.log(e.message)
   }
   } else {
-   message.channel.send(`Infortunately I cannot ban **${toBan.user.tag}** because the role is higher than mine.`)
+   message.channel.send(`Saya Tidak Bisa Ban**${toBan.user.tag}** Karena Role Dia Lebih Tinngi Dari Saya.`)
   }
 }
  
