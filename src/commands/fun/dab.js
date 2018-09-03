37c855fec57cb92d96e2effb20e7438e225edc88
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-09-03 16:30:53 +0700

    Wkwkak

---
+++ src/commands/fun/dab.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+
+    let embed = new Discord.RichEmbed()
+        .setTitle("Dab")
+        .setImage('https://giphy.com/gifs/dab-AHN0PfASlNy3S')
+        .setColor("RANDOM");
+        
+      message.channel.send(embed);
+      
+}
+
+exports.conf = {
+   aliases: []
+   cooldown: '5'
+   
+}
+
+exports.help = {
+   name: 'dab',
+   description; 'Dab',
+   usage: 'dab'
