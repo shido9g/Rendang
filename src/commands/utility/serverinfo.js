e21d547ff99c141b17b4a39995ffa998489eb691
Hazmi35 <hazminoname@gmail.com>
2018-09-03 17:13:34 +0700

    ok

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -2,38 +2,72 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
 
-let sicon = message.guild.iconURL; // kalau server gunakan icon bukan displayAvatar
-            let online = message.guild.members.filter(o => o.presence.status === 'online').size
-		let serverembed = new Discord.RichEmbed()
-		.setAuthor(message.guild.name, sicon)
-		.setThumbnail(sicon)
-		.setColor("RANDOM")
-		.addField("ID", message.guild.id, true)
-		.addField("• Owner", message.guild.owner.user.tag, true)
-		.addField("• Nama Server", message.guild.name)
-		.addField("• Server Dibuat Sejak", message.guild.createdAt) 
-		.addField("• Kamu Join Sejak", message.member.joinedAt)
-                .addField("• Region", message.guild.region, true)
-                .addField("• Channels", message.guild.channels.size, true)
-                .addField("• Jumlah Member", message.guild.memberCount, true)
-                .addField("• Jumlah User", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size, true)
-                .addField("• Jumlah Robot", message.guild.members.filter(m => m.user.bot).size, true)
-                .addField("• Jumlah User Yang Sedang Online", `${online}`, true)
-                .addField("• Jumlah Roles", message.guild.roles.size, true)
-                .setFooter(`Requested By : ${message.author.tag}`);
+  let sicon = message.guild.iconURL;
+  let online = message.guild.members.filter(o => o.presence.status === 'online').size
+  let serverembed = new Discord.RichEmbed()
+    .setAuthor(message.guild.name, sicon)
+    .setThumbnail(sicon)
+    .setColor("RANDOM")
+    .addField("ID", message.guild.id, true)
+    .addField("• Owner", message.guild.owner.user.tag, true)
+    .addField("• Nama Server", message.guild.name)
+    .addField("• Server Dibuat Sejak", message.guild.createdAt)
+    .addField("• Kamu Join Sejak", message.member.joinedAt)
+    .addField("• Region", message.guild.region, true)
+    .addField("• Channels", message.guild.channels.size, true)
+    .addField("• Jumlah Member", message.guild.memberCount, true)
+    .addField("• Jumlah User", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size, true)
+    .addField("• Jumlah Robot", message.guild.members.filter(m => m.user.bot).size, true)
+    .addField("• Jumlah User Yang Sedang Online", `${online}`, true)
+    .addField("• Jumlah Roles", message.guild.roles.size, true)
+    .setFooter(`Requested By : ${message.author.tag}`);
 
-		message.channel.send(serverembed);
-    
-   }
+  message.channel.send(serverembed);
 
-   exports.conf = {
-     aliases: ['server'],
-     cooldown: '5'
-   
-    }
-    
-    exports.help = {
-      name: "serverinfo",
-      description: 'Melihat Info Server',
-      usage: 'serverinfo'
-    }
+  /*Nanti gua ganti jadi serverinfo gua - Hazmi35*/
+
+  /* var verificationLevels = ['**None**', '**Low**', '**Medium**', '**(╯°□°）╯︵ ┻━┻** (High)', '**┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻** (Extreme)'];
+  var region = {
+    "brazi": "**Brazil** :flag_br:",
+    "eu-central": "**Central Europe** :flag_eu:",
+    "singapore": "**Singapore** :flag_sg:",
+    "us-central": "**U.S. Central** :flag_us:",
+    "sydney": "**Sydney** :flag_au:",
+    "us-east": "**U.S. East** :flag_us:",
+    "us-south": "**U.S. South** :flag_us:",
+    "us-west": "**U.S. West** :flag_us:",
+    "eu-west": "**Western Europe** :flag_eu:",
+    "singapore": "**Singapore** :flag_sg:",
+    "london": "**London** :flag_gb:",
+    "japan": "**Japan** :flag_jp:",
+    "russia": "**Russia** :flag_ru:",
+    "hongkong": "**Hong Kong** :flag_hk:"
+  }
+  var embed = new Discord.RichEmbed()
+    .setAuthor(`${message.guild.name}`, message.guild.iconURL)
+    .setDescription(`ID : ${message.guild.id}`)
+    .addField(`Region`, `${region[message.guild.region]}`, true)
+    .setThumbnail(message.guild.iconURL)
+    .setColor(`GOLD`)
+    .addField(`Verification Level`, `${verificationLevels[message.guild.verificationLevel]}`, true)
+    .addField(`Users [${message.guild.members.size}]`, `<:online:313956277808005120> **${message.guild.members.filter(m => m.presence.status === 'online').size}** <:offline:313956277237710868> **${message.guild.members.filter(m => m.presence.status === 'offline').size}** \n<:away:313956277220802560> **${message.guild.members.filter(m => m.presence.status === 'idle').size}** <:dnd:313956276893646850> **${message.guild.members.filter(m => m.presence.status === 'dnd').size}**`, true)
+    .addField(`Channels [**${message.guild.channels.size}**]`, `- **${message.guild.channels.filter(m => m.type === 'category').size}** Category \n- **${message.guild.channels.filter(m => m.type === 'text').size}** Text \n- **${message.guild.channels.filter(m => m.type === 'voice').size}** Voice`, true)
+    .addField(`Roles [${message.guild.roles.size}]`, `Use **${PREFIX}roles** if you want to see the role list`, true)
+    .addField(`Owner`, `${message.guild.owner.user.tag} (${message.guild.ownerID})`)
+    .addField(`Created At`, `${moment(message.guild.createdAt).utcOffset('+0700').format("dddd, MMMM Do YYYY, HH:mm:ss")}`)
+    .setFooter(footer)
+  message.channel.send(embed) */
+
+}
+
+exports.conf = {
+  aliases: ['server'],
+  cooldown: '5'
+
+}
+
+exports.help = {
+  name: "serverinfo",
+  description: 'Melihat Info Server',
+  usage: 'serverinfo'
+}
