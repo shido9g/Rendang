d54e78e7e3c7175dafbfa13020f7ffa6fcf40b7f
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-09-06 11:40:06 +0800

    Fix beberapa typo dan kekurangan spasi

--- src/commands/admin/ban.js
+++ src/commands/admin/ban.js
@@ -1,26 +1,26 @@
 const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, message, args) => {
-  if (!message.member.hasPermission("MANAGE_MESSAGES")){ 
+  if (!message.member.hasPermission("BAN_MEMBERS")){ 
     let embed = new RichEmbed()
       .setColor("RANDOM")
-      .setTitle("Maaf Kamu Tidak Mempunyai Permissions Untuk Ban Members");
+      .setDescription("Maaf Kamu Tidak Mempunyai Permissions Untuk Ban Members");
 return message.channel.send(embed);
   }
-  if (!message.guild.member(client.user).hasPermission("MANAGE_NICKNAMES")) return message.channel.send(`**${message.author.tag}** Maaf, Rendang Tidak Mempunyai Permissions \`BAN_MEMBERS\` Tolong Beri Rendang Permissions Untuk Ban Member :)`).then(msg=>msg.delete(5000))
+  if (!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.channel.send(`**${message.author.tag}** Maaf, Rendang Tidak Mempunyai Permissions \`BAN_MEMBERS\` Tolong Beri Rendang Permissions Untuk Ban Member :)`).then(msg=>msg.delete(5000))
   
   let toBan = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!toBan) return message.channel.sendMessage("Tidak Dapat Menemukan User ! Mention User Terlebih Dahulu !");
+  if(!toBan) return message.channel.sendMessage("Tidak Dapat Menemukan User! Mention User Terlebih Dahulu!");
   let reason = args.join(" ").slice(22);
-  if (toBan.hasPermission("BAN_MEMBERS")) return message.channel.send("User Ini Tidak Dapat Diban :(").then(msg => msg.delete(3000));
+  if (toBan.hasPermission("BAN_MEMBERS")) return message.channel.send("User Ini Tidak Dapat Diban :(").then(msg => msg.delete(5000));
   
   if (toBan.highestRole.position < message.guild.member(client.user).highestRole.position) {
    message.guild.member(toBan).ban(reason);
    try {
     if (!reason) {
-      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari**${message.guild.name}**`)
+      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari **${message.guild.name}**`)
     } else {
-      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari**${message.guild.name}**
+      toBan.send(`**${toBan.user.tag}** Kamu Telah Di Ban Dari **${message.guild.name}**
 Alasan: "${reason}"`);
     }
     let embedB = new RichEmbed()
@@ -33,7 +33,7 @@ Alasan: "${reason}"`);
     console.log(e.message)
   }
   } else {
-   message.channel.send(`Saya Tidak Bisa Ban**${toBan.user.tag}** Karena Role Dia Lebih Tinngi Dari Saya.`)
+   message.channel.send(`Saya Tidak Bisa Ban **${toBan.user.tag}** Karena Role Dia Lebih Tinngi Dari Saya.`)
   }
 }
  
@@ -44,6 +44,6 @@ exports.conf = {
 
 exports.help = {
   name: "ban",
-  description: 'Ban Seseorang Dari Servermu [ADMIN ONLY]',
+  description: 'Ban Seseorang Dari Servermu [PERMISSION BAN MEMBERS ONLY]',
   usage: 'ban [@mention someone]'
 }
