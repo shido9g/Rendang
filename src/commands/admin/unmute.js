31202c1148e62da5b51305e74c929e3c8a32ed0c
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-09-12 11:34:36 +0800

    DeprecationWarning: Collection#find, Oke. Fixed

--- src/commands/admin/unmute.js
+++ src/commands/admin/unmute.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!member) return message.channel.send(`**${message.author.username}**, Maaf aku tidak bisa mencari member yang kamu maksud!`).then(msg=>msg.delete(5000));
     
-    let muterole = message.guild.roles.find('name', 'Muted');
+    let muterole = message.guild.roles.find(x => x.name === 'Muted');
     if (!member.roles.has(muterole.id)) return message.channel.send(`**${message.author.username}**, dia belum ter-mute coba mute dia terlebih dahulu :)`).then(msg=>msg.delete(5000));
     await (member.removeRole(muterole.id));
     message.channel.send(`<@${member.id}> selamat kamu sudah ter-unmute.`);
