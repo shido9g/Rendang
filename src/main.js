73b2e083af3043b0a72cf0d53e4e6b871d127aeb
Hazmi35 <hazminoname@gmail.com>
2018-09-18 20:06:36 +0700

    moving sharding manager file to src folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 
-const shards = new ShardingManager('./index.js', {
+const shards = new ShardingManager('./src/app.js', {
     token: process.env.BOT_TOKEN,
     totalShards: 1
 });
