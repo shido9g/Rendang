e5938fe69e97dfb4b76277850ac93a3c986884c7
King Of Erangel <39506185+kakakikikuku@users.noreply.github.com>
2018-10-26 18:00:51 +0800

    gede gituloh xD

--- src/commands/core/invite.js
+++ src/commands/core/invite.js
@@ -4,8 +4,8 @@ exports.run = async (client, message, args) => {
 
 let embed = new SharifEmbed()
       .setColor("RANDOM")
-      .setTitle("Ingin Menginvite Rendang Ke Server Mu?")
-      .setDescription("[Klik Disini](https://discordapp.com/oauth2/authorize?client_id=485054998632464384&scope=bot&permissions=112327680)")
+      .setAuthor("Ingin Menginvite Rendang Ke Server Mu?")
+      .setDescription("**[Klik Disini](https://discordapp.com/oauth2/authorize?client_id=485054998632464384&scope=bot&permissions=112327680)**")
       .setFooter(`Requested By : ${message.author.username}`);
       
      message.channel.send(embed);
